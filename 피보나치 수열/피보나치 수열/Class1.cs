42f06a61f5b52bb3c530218e076998db9db84cc0
GreeNe1129 <168407912+GreeNe1129@users.noreply.github.com>
2024-05-13 16:48:10 +0900

    Add files via upload

---
+++ 피보나치 수열/피보나치 수열/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 피보나치_수열
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            int d = 0;
+
+            string num = Console.ReadLine();
+
+            for (int i = 0; i <= int.Parse(num); i++)
+            {
+                d = a + b + c;
+                a = b;
+                b = c;
+                c = d;
+
+                Console.WriteLine(d + " ");
+            }
+        }
+    }
+}
